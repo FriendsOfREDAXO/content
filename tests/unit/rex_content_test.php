592fdadcc2537277e45921e602667e14f5f12591
eaCe <webmaster@slaps.de>
2022-09-27 20:49:28 +0200

    chore: add setTemplateContent test

--- tests/unit/rex_content_test.php
+++ tests/unit/rex_content_test.php
@@ -1,4 +1,5 @@
 <?php
+$templateKey = uniqid('template_', false);
 
 beforeEach(function ()
 {
@@ -22,15 +23,22 @@ test('expect template content to be false', function ()
     expect(rex_content::getTemplateContent(99))->toBeFalse();
 });
 
-test('expect template id', function ()
+test('expect template id', function () use ($templateKey)
 {
-    expect(rex_content::createTemplate('Template Name', 'template_key'))->toBeInt();
+    expect(rex_content::createTemplate('Template Name', $templateKey))->toBeInt();
 });
 
-test('expect template key already exists exception', function ()
+test('expect template key already exists exception', function () use ($templateKey)
 {
-    rex_content::createTemplate('Template Name 2', 'template_key');
+    rex_content::createTemplate('Template Name 2', $templateKey);
 })->throws(rex_exception::class, 'Template key already exists');
+
+test('expect to set template content', function () use ($templateKey)
+{
+    $template = rex_template::forKey($templateKey);
+    expect(rex_content::setTemplateContent($template->getId(), 'Lorem Ipsum'))
+        ->not->toThrow(rex_sql_exception::class);
+});
 
 /**
  * article
