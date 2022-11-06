aaf20c8a128e50a276284aa840659217ba92f886
eaCe <webmaster@slaps.de>
2022-11-06 20:49:40 +0100

    feat: add get basic template method [skip ci]

---
+++ lib/content_template.php
@@ -0,0 +1,25 @@
+<?php
+
+class content_template
+{
+    /**
+     * @param string $title
+     * @return string
+     */
+    public function get(string $title = ''): string
+    {
+        return '
+        <!doctype html>
+        <html class="no-js" lang="">
+            <head>
+              <meta charset="utf-8">
+              <title>' . $title . '</title>
+              <meta name="description" content="">
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+            </head>
+            <body>
+                REX_ARTICLE[]
+            </body>
+        </html>';
+    }
+}
